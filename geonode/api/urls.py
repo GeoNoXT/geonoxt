a58e070f629ac857d84dfdf4c39742f265fd0b0f
Niji <mathereall@gmail.com>
2024-08-30 23:10:30 -0600

    group_list_public

--- geonode/api/urls.py
+++ geonode/api/urls.py
@@ -27,7 +27,7 @@ from . import resourcebase_api as resourcebase_resources
 
 api = Api(api_name="api")
 
-"""
+
 api.register(resources.GroupCategoryResource())
 api.register(resources.GroupResource())
 api.register(resources.GroupProfileResource())
@@ -43,7 +43,6 @@ api.register(resourcebase_resources.FeaturedResourceBaseResource())
 api.register(resourcebase_resources.LayerResource())
 api.register(resourcebase_resources.MapResource())
 api.register(resourcebase_resources.GeoAppResource())
-"""
 api.register(resourcebase_resources.ResourceBaseResource())
 
 router = routers.DynamicRouter()
