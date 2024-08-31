53b3e82c785be24ce787282d6f2f2fb2065067fb
Niji <mathereall@gmail.com>
2024-08-31 13:20:30 -0600

    group_list_public

--- geonode/api/views.py
+++ geonode/api/views.py
@@ -31,7 +31,7 @@ from oauth2_provider.exceptions import OAuthToolkitError, FatalClientError
 from allauth.account.utils import user_field, user_email, user_username
 
 from ..utils import json_response
-from ..decorators import superuser_or_apiauth
+from ..decorators import superuser_or_apiauth, logged_in_or_apiauth
 from ..base.auth import get_token_object_from_session, get_auth_token
 
 
@@ -54,6 +54,7 @@ def verify_access_token(request, key):
 
 
 @csrf_exempt
+@logged_in_or_apiauth()
 def user_info(request):
     user = request.user
 
