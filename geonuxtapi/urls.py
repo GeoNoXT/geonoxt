cb422bc61a007c77b2a51c04f198a21595eed22f
Niji Saeba Media Noche <mathereall@gmail.com>
2024-09-06 14:26:37 -0600

    OIDC_RSA_PRIVATE_KEY de 2048

--- geonuxtapi/urls.py
+++ geonuxtapi/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
 
-from .views import UserInfoView
+from .views import UserInfoView, DirectLogoutView
 
 urlpatterns = [
     path("geonuxt/userinfo", UserInfoView.as_view(), name="userinfo"),
-    path("geonuxt/logout", UserInfoView.as_view(), name="userinfo"),
+    path("geonuxt/logout", DirectLogoutView.as_view(), name="logout"),
 ]
