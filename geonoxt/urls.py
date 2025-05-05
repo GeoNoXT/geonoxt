0ceb2fb85d1f68d26facc8e3494fea0c4979aab7
Niji <mathereall@gmail.com>
2025-05-05 01:16:07 -0600

    Fix path formatting for cloud task run job endpoint in urls.py

--- geonoxt/urls.py
+++ geonoxt/urls.py
@@ -4,5 +4,5 @@ from geonoxt.views import cloud_task_run_job
 urlpatterns = [
     # path("", include("geonoxt.xt_br.urls")),
     # path("", include("geonoxt.xt_geoserver.urls")),
-    path('/api/v2/management-tasks/cloud-task-run-job/', cloud_task_run_job, name='cloud-task-run-job'),
+    path('api/v2/management-tasks/cloud-task-run-job/', cloud_task_run_job, name='cloud-task-run-job'),
 ]
